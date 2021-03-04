d1b82ae6d66a44e16d2edd4dfb3c00570c910d97
SiarheiBalt <75533283+SiarheiBalt@users.noreply.github.com>
2021-03-04 16:58:11 +0300

    dz_10, fix_DZ_9

--- 2021_02_22-9lesson/Part2/index.js
+++ 2021_02_22-9lesson/Part2/index.js
@@ -12,18 +12,17 @@ buttonSelect.addEventListener('click', function () {
     };
     let mainRemove = document.querySelector('main');
     mainRemove.innerHTML = '';
-   
+
     // 'http://www.omdbapi.com/?apikey=14ec9722&s=war&t=movie&4'
     // Чтоб выводил 10 результатов -'s' вместо 't'
 
     let url = `http://www.omdbapi.com/?apikey=14ec9722&s=${inputAdd.value}&t=${selectTypeVideo}&4`;
     let data = fetch(url)
-    .then ((response)=> {
-        return response.json();
-    })
-    .then((data) => {
-        if (data.Search) {
-
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            if (data.Search) {
                 data['Search'].forEach(el => {
 
                     let divV = document.createElement('div');
@@ -47,57 +46,72 @@ buttonSelect.addEventListener('click', function () {
 
                     buttonV.addEventListener('click', function () {
                         let footerRemove = document.querySelector('footer');
-                        
-                            footerRemove.innerHTML = '';
-                            creatEllInfo(el.imdbID)
+
+                        footerRemove.innerHTML = '';
+                        creatEllInfo(el.imdbID)
                     })
                 });
+            } else {
+                let divNotFound = document.createElement('h2');
+                divNotFound.textContent = 'Sorry, video not found :( ';
+                document.querySelector('main').append(divNotFound);
             }
-        })   
+        })
+        .catch(err => {
+            let errMessage = document.createElement('h1');
+            errMessage.textContent = 'Error: server not found! '
+            let main = document.querySelector('main');
+            main.append(errMessage);
+
+        })
+        .finally(() => {
+            console.log('finally');
+        })
 });
 
 function creatEllInfo(items) {
 
     let url = `http://www.omdbapi.com/?apikey=14ec9722&i=${items}`;
     let data = fetch(url)
-    .then ((response)=> {
-        return response.json();
-    })
-    .then((data) => { {
-        if(data) {
-            let footer = document.querySelector('footer');
-            let imgDetails = document.createElement('img');
-            imgDetails.src = data.Poster;
-            let divDetails = document.createElement('div');
-            let titleDetails = document.createElement('h3');
-            titleDetails.textContent = data.Title;
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            {
+                if (data) {
+                    let footer = document.querySelector('footer');
+                    let imgDetails = document.createElement('img');
+                    imgDetails.src = data.Poster;
+                    let divDetails = document.createElement('div');
+                    let titleDetails = document.createElement('h3');
+                    titleDetails.textContent = data.Title;
 
-            let relised = document.createElement('h4');
-            relised.textContent = data.Released;
+                    let relised = document.createElement('h4');
+                    relised.textContent = data.Released;
 
-            let genre = document.createElement('h4');
-            genre.textContent = data.Genre;
+                    let genre = document.createElement('h4');
+                    genre.textContent = data.Genre;
 
-            let country = document.createElement('h4');
-            country.textContent = data.Country;
+                    let country = document.createElement('h4');
+                    country.textContent = data.Country;
 
-            let director = document.createElement('h4');
-            director.textContent = data.Director;
+                    let director = document.createElement('h4');
+                    director.textContent = data.Director;
 
-            let writer = document.createElement('h4');
-            writer.textContent = data.Writer;
+                    let writer = document.createElement('h4');
+                    writer.textContent = data.Writer;
 
-            let actors = document.createElement('h5');
-            actors.textContent = data.Actors;
+                    let actors = document.createElement('h5');
+                    actors.textContent = data.Actors;
 
-            let awards = document.createElement('h4');
-            awards.textContent = data.Awards;
-            
-            divDetails.style.width = '600px';
-            divDetails.append(titleDetails,  relised, genre, country, director, writer, actors, awards)
-            footer.append(imgDetails, divDetails);
-        }    
-    }
-})
+                    let awards = document.createElement('h4');
+                    awards.textContent = data.Awards;
+
+                    divDetails.style.width = '600px';
+                    divDetails.append(titleDetails, relised, genre, country, director, writer, actors, awards)
+                    footer.append(imgDetails, divDetails);
+                }
+            }
+        })
 }
 let mainTag = document.querySelector('main');
