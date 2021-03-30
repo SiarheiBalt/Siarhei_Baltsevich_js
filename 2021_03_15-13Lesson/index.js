3eb330cfd7d9eb93e3b8b62a08854358081a3565
SiarheiBalt <75533283+SiarheiBalt@users.noreply.github.com>
2021-03-30 20:22:33 +0300

    Add enter, backspace, arrows.

--- 2021_03_15-13Lesson/index.js
+++ 2021_03_15-13Lesson/index.js
@@ -1,42 +1,92 @@
 const bordKeys = {
-    ru : 'йцукенгшщзхъфывапролджэячсмитьбю',
-    eng : `qwertyuiop[]asdfghjkl;'zxcvbnm,./`,
-    numbers: '1234567890-=',
-    shiftNumbers: '?!@#$%^*()_+',
+    ru: 'йцукенгшщзхъфывапролджэячсмитьбю↵',
+    eng: `qwertyuiop[]asdfghjkl;'zxcvbnm,./↵`,
+    numbers: '1234567890-=⟵',
+    shiftNumbers: '?!@#$%^*()_+⟵',
     caps: 'caps lock',
     shift: 'shift',
     alt: 'alt',
+    spaceAndArrows: ' ᐸᐱᐯᐳ'
 }
 stateSomeKeys = {
-    'caps lock' : false,
-    shift : false,
-    alt : false,
+    'caps lock': false,
+    shift: false,
+    alt: false,
 }
 const main = document.querySelector('main');
 const aside = document.querySelector('aside');
 const footer = document.querySelector('footer');
 const header = document.querySelector('header');
 const textArea = document.querySelector('#textArea');
+textArea.focus()
 
 createElAddEvent(bordKeys.ru.split(''), main);
 createElAddEvent(bordKeys.numbers.split(''), header);
 createElAddEvent([bordKeys.caps, bordKeys.shift, bordKeys.alt], aside);
+createElAddEvent(bordKeys.spaceAndArrows.split(''), footer)
 function createElAddEvent(arr, place) {
     arr.forEach((el) => {
         let button = document.createElement('button');
         button.innerHTML = el;
+        changeStyleButton(button);
         button.onclick = handler;
-        place.append(button); 
+        place.append(button);
     })
+}
+function changeStyleButton(button) {
+    if (button.innerHTML === ' ') button.classList.add('back_space');
+    if (button.innerHTML === 'ᐱ') button.classList.add('upArrow');
+    if (button.innerHTML === '⟵') button.style.width = '40px';
+    if (button.innerHTML === '↵') button.classList.add('enter');
 }
 
 function handler(event) {
-    if(event.target.innerHTML.split('').length > 1) {
-        return keyDownUp(event)
-    }
-    stateSomeKeys['caps lock'] ===true || stateSomeKeys.shift ===true ? 
-        textArea.innerHTML += event.target.innerHTML.toUpperCase() : 
+    let key = event.target.innerHTML;
+    if (key === '↵') return enterEvent()
+    if (key === '⟵') return backSpaceEvent()
+    if (key.split('').length > 1) return keyDownUp(event);
+    if (key === 'ᐸ' || key ==='ᐱ' || key ==='ᐯ' || key ==='ᐳ') return arrowsEvent(key)
+    stateSomeKeys['caps lock'] === true || stateSomeKeys.shift === true ?
+        textArea.innerHTML += event.target.innerHTML.toUpperCase() :
         textArea.innerHTML += event.target.innerHTML.toLowerCase();
+        return focusCount = 0;
+}
+function arrowsEvent(key) {
+    if(key === 'ᐸ') {
+        focusCount++;
+        moveFocus(focusCount);
+        return focusCount;
+    }
+    if(key === 'ᐳ') {
+        focusCount--;
+        moveFocus(focusCount);
+        return focusCount;
+    }
+    if(key === 'ᐱ') {
+        focusCount = textArea.innerHTML.length;
+        moveFocus(focusCount);
+    }
+    if(key === 'ᐯ') {
+        focusCount = 0;
+        moveFocus(focusCount);
+    }
+    
+}
+let focusCount = 0;
+
+function enterEvent() {
+    textArea.innerHTML = textArea.innerHTML + '\n';
+}
+function moveFocus(c) {
+    textArea.focus();
+    textArea.setSelectionRange(textArea.innerHTML.length, textArea.innerHTML.length - c);
+   
+}
+function backSpaceEvent() {
+    let text = textArea.innerHTML.split('');
+    text.splice(text.length - 1, 1);
+    textArea.innerHTML = textArea.innerHTML.split().pop()
+    textArea.innerHTML = text.join('');
 }
 function keyDownUp(event) {
     stateSomeKeys[event.target.innerHTML] = !stateSomeKeys[event.target.innerHTML];
@@ -47,7 +97,7 @@ function keyDownUp(event) {
     return stateSomeKeys[event.target.innerHTML];
 }
 function addDellBacklightSomeKeys(event) {
-    !event.path[0].classList.contains('backLight') ? 
+    !event.path[0].classList.contains('backLight') ?
         event.path[0].classList.add('backLight') :
         event.path[0].classList.remove('backLight')
 }
