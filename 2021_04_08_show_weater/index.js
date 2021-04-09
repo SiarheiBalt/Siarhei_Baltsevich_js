999cc444670cf40c549f2bba4ab82bbbe71effec
SiarheiBalt <75533283+SiarheiBalt@users.noreply.github.com>
2021-04-09 10:38:56 +0300

    async await

--- 2021_04_08_show_weater/index.js
+++ 2021_04_08_show_weater/index.js
@@ -4,27 +4,23 @@ function findDiv(...arr) {
 };
 
 button.addEventListener('click', load)
+document.querySelector('#wr').addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') load()
+})
 
-function load(event) {
+async function load() {
     const city = input.value;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},&APPID=936c7b67f815746ef93d514f2b8c3b89`;
 
-    fetch(url)
-        .then((response) => {
-            return response.json()
-        })
-        .then((data) => {
-            let weather = {
-                temp: Math.round(data.main.temp - 273.15),
-                country: data.sys.country,
-            }
-            return weather;
-        })
-        .then((data) => {
-            header.innerHTML = `${data.temp} celsius, ${data.country}`
-
-        })
-        .catch(() => {
-            header.innerHTML = 'Please, enter correct city.'
-        })
+    let response = await fetch(url)
+    try {
+        let data = await response.json()
+        let weather = {
+            temp: Math.round(data.main.temp - 273.15),
+            country: data.sys.country,
+        }
+        header.innerHTML = `${weather.temp} °C, ${weather.country}`
+    } catch {
+        header.innerHTML = 'Please, enter correct city.'
+    }
 }
